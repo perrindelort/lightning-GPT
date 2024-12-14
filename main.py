7e81c34de894b442d4a5a58bed72f26b8b662b09
Antoine Perrin--Delort <103105422+perrindelort@users.noreply.github.com>
2024-12-14 23:13:02 +0100

    Added callbacks and better files handling
    Logger now instantiated instead of being made by the trainer ;
    EarlyStopping implementedd ;
    Model summary added and model's weights saved in results/{model_name}/version_{version})

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import torch
 from pathlib import Path
 from lightning import Trainer, seed_everything
 
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint, RichModelSummary, RichProgressBar
+
 from modules import TinyShakeSpeare, BigramLanguageModel, GPT
 from utils import ConfigParser
 
@@ -22,8 +25,35 @@ if __name__ == "__main__":
     results_dir = Path(__file__).parent / "results"
     if not results_dir.exists():
         results_dir.mkdir()
+
+    logs_dir = results_dir / model._get_name()
+
+    logger = TensorBoardLogger(
+        save_dir=logs_dir,
+        name="",
+        log_graph=True,
+    )
+
+    callbacks = [
+        EarlyStopping(
+            monitor="val_loss",
+            patience=5,
+            verbose=True,
+        ),
+        # LearningRateMonitor,
+        ModelCheckpoint(
+            dirpath=logger.log_dir,  # Contains the eventual sub_directories created
+            filename="{epoch}-{val_loss:.3f}",
+            monitor="val_loss",
+            verbose=True,
+        ),
+        RichModelSummary(),
+        RichProgressBar(),
+    ]
+
     trainer = Trainer(
-        default_root_dir=results_dir / model._get_name(),
+        logger=logger,
+        callbacks=callbacks,
         max_epochs=config.max_epochs,
         max_steps=config.max_steps,
         limit_train_batches=config.eval_interval,
