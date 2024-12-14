e0349415078a1f76350c3e4e37bd441d449fe848
Antoine Perrin--Delort <103105422+perrindelort@users.noreply.github.com>
2024-12-14 14:38:21 +0100

    Edited comments

--- layers/head.py
+++ layers/head.py
@@ -11,9 +11,7 @@ class Head(nn.Module):
     One head of self-attention
     """
 
-    def __init__(
-        self, head_size: int, n_embeddings: int, block_size: int, dropout: Optional[float] = 0.0
-    ):
+    def __init__(self, head_size: int, n_embeddings: int, block_size: int, dropout: Optional[float] = 0.0):
         super().__init__()
         self.key = nn.Linear(n_embeddings, head_size, bias=False)
         self.query = nn.Linear(n_embeddings, head_size, bias=False)
@@ -23,16 +21,14 @@ class Head(nn.Module):
 
     def forward(self, x):
         B, T, C = x.shape
-        k = self.key(x)  # (B, T, C)
-        q = self.query(x)
+        k = self.key(x)  # (B, T, hs)
+        q = self.query(x)  # (B, T, hs)
         # Compute attention score "affinities"
-        w = q @ rearrange(k, "b t c -> b c t") * C**-0.5  # (B, T, C) @ (B, C, T) -> (B, T, T)
-        w = w.masked_fill(
-            self.tril[:T, :T] == 0, float("-inf")
-        )  # (B, T, T) lower triangular "exponentially-wise"
+        w = q @ rearrange(k, "b t c -> b c t") * k.shape[-1] ** -0.5  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        w = w.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T) lower triangular "exponentially-wise"
         w = F.softmax(w, dim=-1)  # (B, T, T)
         w = self.dropout(w)
         # Perform the weighted aggregation of the values
-        v = self.value(x)  # (B, T, C)
-        out = w @ v  # (B, T, T) @ (B, T, C) -> (B, T, C)
+        v = self.value(x)  # (B, T, hs)
+        out = w @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
